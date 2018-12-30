dcd171a0f774cabc3b99696e7b8f6a98e74da278
Phillip Schichtel <phillip@schich.tel>
2018-12-31 00:35:44 +0100

    move the test functions comment over to the test action

--- src/main/scala/tel/schich/virtualecu/Main.scala
+++ src/main/scala/tel/schich/virtualecu/Main.scala
@@ -48,10 +48,6 @@ object Main {
         Integer.parseUnsignedInt(n, 16)
     }
 
-//    def positiveSine(period: FiniteDuration): Double => Double = lag(period / 4) andThen sine(period) andThen shift(1) andThen scale(0.5)
-//    val coolantTemperature = add(linear(1, 8.minutes), gaussianRandom(-0.001, 0.001)) andThen lowCut(-40) andThen combined(90.0, scale, highCut) andThen shift(40)
-//    val engineRpm = add(product(positiveSine(5.minute), square(10.minute) andThen shift(1) andThen scale(0.5)) andThen scale(6000.0), positiveSine(10.minute)) andThen scale(6000.0)
-
     def startEmulation(interfaceName: String, conf: Path): Unit = {
         val eval = new Eval()
         val emulationSpec = loadConf(conf)
